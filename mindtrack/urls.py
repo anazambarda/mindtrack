fbb74e4c70866a0615785ff2f922aafd96c714fd
Ana Zambarda <anacarolinazambarda@gmail.com>
2025-04-06 00:46:38 -0300

    Feito o login

--- mindtrack/urls.py
+++ mindtrack/urls.py
@@ -3,5 +3,6 @@ from projeto_mindtrack import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('cadastro/', views.cadastro, name = 'cadastro')
+    path('cadastro/', views.cadastro, name = 'cadastro'),
+    path('login/', views.login, name = 'login')
 ]
--- projeto_mindtrack/views.py
+++ projeto_mindtrack/views.py
@@ -5,7 +5,7 @@ from .forms import CadastroForm
 def home(request):
     return render(request, 'login/home.html')
 
-def cadastro(request):
+def cadastro(request):  
     if request.method == 'POST':
         form = CadastroForm(request.POST)
         if form.is_valid():
@@ -15,3 +15,16 @@ def cadastro(request):
         form = CadastroForm()
     
     return render(request, 'cadastro/cadastro.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+        try:
+            usuario = Usuario.objects.get(email=email, senha=senha)
+            return redirect('home')  # redireciona para uma página de sucesso
+        except Usuario.DoesNotExist:
+            return render(request, 'login/home.html', {'error': 'Email ou senha inválidos.'})
+    
+    return render(request, 'login/home.html') # colocar pra onde vai redirecionar quando der sucesso
